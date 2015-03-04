35987dfb73cde94fa747ce43124137fd4b41aa61
Vincent Mimoun-Prat <vpratfr@gmail.com>
2015-03-04 12:01:55 +0100

    potomo file works

--- grunt/tasks/potomo.js
+++ grunt/tasks/potomo.js
@@ -1,17 +1,42 @@
 module.exports = function (grunt, options) {
-    return {
-        dist: {
-            options: {
-                poDel: false // Set to true if you want to erase the .po
-            },
-            files: [{
-                expand: true,
-                cwd: options.i18n.poPath,
-                src: ['*.po'],
-                dest: options.i18n.poPath,
-                ext: '.mo',
-                nonull: true
-            }]
+    var extend = require('extend');
+
+    // The options that are common to all plugins
+    var baseOptions = {
+        options: {
+            poDel: false // Set to true if you want to erase the .po
         }
     };
+
+    var targets = {};
+
+    // Create the targets for the base plugin and all add-ons
+    targets["customer-area"] = extend(true, {}, baseOptions, {
+        options: {},
+        files: [{
+            expand: true,
+            src: ['*.po'],
+            ext: '.mo',
+            nonull: true,
+            cwd: options.paths.base_plugin + '/languages',
+            dest: options.paths.base_plugin + '/languages'
+        }]
+    });
+
+    var addons = options.addons;
+    addons.forEach(function (addon) {
+        targets[addon.slug] = extend(true, {}, baseOptions, {
+            options: {},
+            files: [{
+                expand: true,
+                src: ['*.po'],
+                ext: '.mo',
+                nonull: true,
+                cwd: addon.path + '/' + addon.langFolder,
+                dest: addon.path + '/' + addon.langFolder
+            }]
+        });
+    });
+
+    return targets;
 };
