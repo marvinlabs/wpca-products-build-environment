03dfd2445d138c7538d9e66dd0582dfb92878ff1
Thomas Lartaud <contact@thomaslartaud.com>
2015-03-16 17:02:58 +0100

    vagrant-provision.bat should be run the first time instead of vagrant-start.bat

--- vagrant-start.bat
+++ vagrant-start.bat
@@ -1,5 +1,9 @@
-cd vagrant
-vagrant up
-cd ..
-start http://wpca.dev
-grunt start-dev
+if[ ! -f "vagrant/www/default/dashboard-custom.php" ]; then
+   vagrant-provision.bat
+else
+   cd vagrant
+   vagrant up
+   cd ..
+   start http://wpca.dev
+   grunt start-dev
+fi
