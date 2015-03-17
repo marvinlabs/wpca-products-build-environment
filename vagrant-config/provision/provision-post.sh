7527568182ec10f7e65fdee2bba6d58daf43d22f
Thomas Lartaud <contact@thomaslartaud.com>
2015-03-17 11:44:14 +0100

    create symlinks for WP Default, Develop and Trunk

--- vagrant-config/provision/provision-post.sh
+++ vagrant-config/provision/provision-post.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 
-path="/srv/www/wordpress-default/wp-content"
+paths=( "/srv/www/wordpress-default/wp-content" "/srv/www/wordpress-develop/src/wp-content" "/srv/www/wordpress-trunk/wp-content" )
 
-# Check if plugins symlink exists
-cd $path
-if [ -d "plugins" ]; then
-  rm -R "plugins"
-fi
-if [ ! -d "plugins" ] && [ ! -f "plugins" ] && [ ! -h "plugins" ]; then
-  ln -sf "/srv/wpca-plugins" "plugins"
-fi
+for i in "${paths[@]}"
+do
+	# Check if plugins symlink exists
+    cd $i
+    if [ -d "plugins" ]; then
+      rm -R "plugins"
+    fi
+    if [ ! -d "plugins" ] && [ ! -f "plugins" ] && [ ! -h "plugins" ]; then
+      ln -sf "/srv/wpca-plugins" "plugins"
+    fi
 
-# Check if themes symlink exists
-cd $path
-if [ -d "themes" ]; then
-  rm -R "themes"
-fi
-if [ ! -d "themes" ] && [ ! -f "themes" ] && [ ! -h "themes" ]; then
-  ln -sf "/srv/wpca-themes" "themes"
-fi
+    # Check if themes symlink exists
+    cd $i
+    if [ -d "themes" ]; then
+      rm -R "themes"
+    fi
+    if [ ! -d "themes" ] && [ ! -f "themes" ] && [ ! -h "themes" ]; then
+      ln -sf "/srv/wpca-themes" "themes"
+    fi
+done
