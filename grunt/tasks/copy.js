0748c01efced75bb90701ab9f3ee9a272da64abd
Vincent Mimoun-Prat <vpratfr@gmail.com>
2015-03-31 11:49:07 +0200

    Allow specifying excluded works + fix class replacement in :not() selectors

--- grunt/tasks/copy.js
+++ grunt/tasks/copy.js
@@ -1,24 +1,28 @@
 module.exports = function (grunt, options) {
-    var rgx = /(\.)([^\d]\w[^\s"\.]*)([\(,\s;\.])/g;
+    var rgx = /(\.)([^\d]\w[^\s"\.:]*)([\(,\s;:\.])/g;
     var tildeRgx = /\.col-/g;
     var prefix = 'cuar-';
+    var exclusions = ['Microsoft'];
 
     return {
         'copy-bootstrap': {
             cwd: 'vendor/bower/',
             expand: true,
-            src: ['bootstrap/*', 'bootstrap/**/*', '!bootstrap/**/*.less'],
+            src: ['bootstrap/*', 'bootstrap/**/*', '!bootstrap/**/*.less', '!bootstrap/**/*.css'],
             dest: 'vendor/wpca'
         },
         'prefix-bootstrap': {
             cwd: 'vendor/bower/',
             expand: true,
-            src: ['bootstrap/**/*.less'],
+            src: ['bootstrap/**/*.less', 'bootstrap/**/*.css'],
             dest: 'vendor/wpca',
             options: {
                 process: function (content, srcpath) {
                     content = content.replace(rgx, "$1" + prefix + "$2$3");
                     content = content.replace(tildeRgx, "." + prefix + "col-");
+                    exclusions.forEach(function(str) {
+                       content = content.replace(prefix+str, str);
+                    });
                     return content;
                 }
             },
