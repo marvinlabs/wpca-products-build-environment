96388cc0db3a9cb0899838ddf4eb7e3075415354
Thomas Lartaud <contact@thomaslartaud.com>
2015-05-20 15:53:32 +0200

    copy missing defaults wp themes prior to symlink creation

--- vagrant-config/provision/provision-post.sh
+++ vagrant-config/provision/provision-post.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 for i in "/srv/www/wordpress-default/wp-content" "/srv/www/wordpress-develop/src/wp-content" "/srv/www/wordpress-trunk/wp-content"; do
-	# Check if plugins symlink exists
+
+    # Check if plugins symlink exists
     cd $i
     if [ -d "plugins" ]; then
       rm -R "plugins"
@@ -11,11 +12,19 @@ for i in "/srv/www/wordpress-default/wp-content" "/srv/www/wordpress-develop/src
     fi
 
     # Check if themes symlink exists
-    cd $i
-    if [ -d "themes" ]; then
-      rm -R "themes"
-    fi
-    if [ ! -d "themes" ] && [ ! -f "themes" ] && [ ! -h "themes" ]; then
-      ln -sf "/srv/wpca-themes" "themes"
+    if [ ! -h "themes" ]; then
+        cd $i
+        if [ -d "themes" ]; then
+            for D in `find themes -mindepth 1 -maxdepth 1 -type d`
+            do
+                if [ ! -d "/srv/wpca-themes/"$D ]; then
+                    mv $D "/srv/wpca-themes/"$D
+                fi
+            done
+            rm -R themes
+        fi
+        if [ ! -d "themes" ] && [ ! -f "themes" ]; then
+          ln -sf "/srv/wpca-themes" "themes"
+        fi
     fi
 done
