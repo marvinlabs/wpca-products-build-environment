5302718a234b28d1ed2ba58a6b6fc9918aa1ff99
Thomas Lartaud <contact@thomaslartaud.com>
2015-05-20 16:16:20 +0200

    unify provision script syntax

--- vagrant-config/provision/provision-post.sh
+++ vagrant-config/provision/provision-post.sh
@@ -1,30 +1,29 @@
 #!/bin/bash
 
+# Script that create required
+# symlinks to work with
+# WPCA environment
+
 for i in "/srv/www/wordpress-default/wp-content" "/srv/www/wordpress-develop/src/wp-content" "/srv/www/wordpress-trunk/wp-content"; do
 
-    # Check if plugins symlink exists
-    cd $i
-    if [ -d "plugins" ]; then
-      rm -R "plugins"
-    fi
-    if [ ! -d "plugins" ] && [ ! -f "plugins" ] && [ ! -h "plugins" ]; then
-      ln -sf "/srv/wpca-plugins" "plugins"
-    fi
+    for type in "plugins" "themes"; do
 
-    # Check if themes symlink exists
-    if [ ! -h "themes" ]; then
-        cd $i
-        if [ -d "themes" ]; then
-            for D in `find themes -mindepth 1 -maxdepth 1 -type d`
-            do
-                if [ ! -d "/srv/wpca-"$D ]; then
-                    mv $D "/srv/wpca-"$D
-                fi
-            done
-            rm -R themes
+        if [ ! -h ${type} ]; then
+            cd ${i}
+            if [ -d ${type} ]; then
+                for D in `find ${type} -mindepth 1 -maxdepth 1 -type d`
+                do
+                    if [ ! -d "/srv/wpca-"${D} ]; then
+                        mv ${D} "/srv/wpca-"${D}
+                    fi
+                done
+                rm -R ${type}
+            fi
+            if [ ! -d ${type} ] && [ ! -f ${type} ]; then
+              ln -sf "/srv/wpca-"${type} ${type}
+            fi
         fi
-        if [ ! -d "themes" ] && [ ! -f "themes" ]; then
-          ln -sf "/srv/wpca-themes" "themes"
-        fi
-    fi
+
+    done
+
 done
