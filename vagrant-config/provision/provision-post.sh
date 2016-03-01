3bde50891539b8932672450067ad06ac707fc4e7
Thomas Lartaud <contact@thomaslartaud.com>
2016-03-01 15:09:37 +0100

    New symlinks to make sourcemap to work. Require vagrant-rebuild.

--- vagrant-config/provision/provision-post.sh
+++ vagrant-config/provision/provision-post.sh
@@ -30,3 +30,28 @@ for i in "/srv/www/wordpress-default/wp-content" "/srv/www/wordpress-develop/src
     done
 
 done
+
+for i in "/srv/www/wordpress-default" "/srv/www/wordpress-develop/src" "/srv/www/wordpress-trunk"; do
+
+    cd ${i}
+
+    for type in "vendor"; do
+
+        if [ ! -h ${type} ]; then
+            if [ -d ${type} ]; then
+                for D in `find ${type} -mindepth 1 -maxdepth 1 -type d`
+                do
+                    if [ ! -d "/srv/wpca-"${D} ]; then
+                        mv ${D} "/srv/wpca-"${D}
+                    fi
+                done
+                rm -R ${type}
+            fi
+            if [ ! -d ${type} ] && [ ! -f ${type} ]; then
+              ln -sf "/srv/wpca-"${type} ${type}
+            fi
+        fi
+
+    done
+
+done
