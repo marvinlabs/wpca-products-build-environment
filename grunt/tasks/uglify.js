dc6195e51ebbdeea497efeec5225e8c3d4c526cf
Thomas Lartaud <Thomas Lartaud>
2016-05-03 18:13:37 +0200

    fix again

--- grunt/tasks/uglify.js
+++ grunt/tasks/uglify.js
@@ -35,7 +35,7 @@ module.exports = function (grunt, options) {
         targets[addon.slug + '-frontend'] = extend(true, {}, baseOptions(
                 {
                     sourceMapName: addon.path + "/assets/frontend/js/" + addon.slug + ".js.map",
-                    sourceMapRoot: addon.path + "/src/js"
+                    sourceMapRoot: addon.path + "/src/js/frontend"
                 }),
             {
                 files: files
@@ -51,7 +51,7 @@ module.exports = function (grunt, options) {
         targets[addon.slug + '-admin'] = extend(true, {}, baseOptions(
                 {
                     sourceMapName: addon.path + "/assets/admin/js/" + addon.slug + ".js.map",
-                    sourceMapRoot: addon.path + "/src/js"
+                    sourceMapRoot: addon.path + "/src/js/admin"
                 }),
             {
                 files: files
