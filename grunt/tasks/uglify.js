9f02ad367aa2e9c8acf3841ec52618381fe43b87
Thomas Lartaud <contact@thomaslartaud.com>
2016-06-30 12:28:41 +0200

    improve uglify task

--- grunt/tasks/uglify.js
+++ grunt/tasks/uglify.js
@@ -95,6 +95,54 @@ module.exports = function (grunt, options) {
                 files: files
             }
         );
+
+        // Custom JS files compiler
+        var customJsFilesFolder = theme.path + '/dev/src/js/custom',
+            jsFilesList = [],
+            customFile = [];
+
+        if(grunt.file.exists(customJsFilesFolder)) {
+
+            // Each skin can contain multiple less files to compile
+            // Dynamically read JS source folder and store paths
+            // JS files names must be : _{myFile}.js
+            grunt.file.recurse(customJsFilesFolder, function (abspath, rootdir, subdir, filename) {
+                if (filename.match(/^_[0-9a-zA-Z\-_.]*\.js$/g)) {
+                    var name = filename.substring(0, filename.lastIndexOf('.')).slice(1).replace('_', '');
+                    jsFilesList.push({
+                        name: name,
+                        abspath: abspath,
+                        rootdir: rootdir,
+                        subdir: subdir,
+                        filename: filename
+                    });
+                }
+            });
+
+            // Create options for all registered JS files
+            for (var i = 0; i < jsFilesList.length; i++) {
+
+                customFile = [];
+                customFile['explodedName'] = jsFilesList[i].name.split('.');
+                customFile['name'] = customFile['explodedName'].slice(-1)[0];
+                customFile['toPath'] = customFile['explodedName'].join('/');
+                
+                files = {};
+                files[theme.path + '/' + customFile['toPath'] + '.min.js'] = [
+                    jsFilesList[i].abspath
+                ];
+
+                targets['cuar-theme-' + theme.slug + '-custom-' + customFile['name']] = extend(true, {}, baseOptions(
+                    {
+                        sourceMapName: theme.path + '/' + customFile['toPath'] + ".js.map",
+                        sourceMapRoot: theme.path + '/dev/src/js/custom/'
+                    }),
+                    {
+                        files: files
+                    }
+                );
+            }
+        }
     });
 
     targets["libs-assets"] = extend(true, {}, {}, options.assets.cuarUglify);
